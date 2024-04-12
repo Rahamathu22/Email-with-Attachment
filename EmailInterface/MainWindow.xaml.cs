e52922a46ef1c85df0ef832fa7417bc887274a49
Rahamath22 <rahamath22104@gmail.com>
2024-04-12 11:49:50 +0530

    Email semding with attachments

---
+++ EmailInterface/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EmailInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student s = new Student();
+        List<Student> stu = new List<Student>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            stu.Add(new Student { Name = "Jaffersha", RollNo = 101, Mailid = "jaffersha786@gmail.com", Message = "Hello" });
+            stu.Add(new Student { Name = "Harish", RollNo = 102, Mailid = "harish12ser@gmail.com", Message = "Hello" });
+           
+        }
+
+        private void mybtn_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Rahamath.IsChecked==true)
+            {
+                foreach (Student S in stu)
+                {
+                    if (S.RollNo == 101)
+                    {
+                        s.Mail(S.Mailid, S.Message, S.Name, S.RollNo);
+                    }
+                }
+            }
+
+            if (Harish.IsChecked == true)
+            {
+                foreach (Student S in stu)
+                {
+                    if (S.RollNo == 102)
+                    {
+                        s.Mail(S.Mailid, S.Message, S.Name, S.RollNo);
+                    }
+                }
+            }
+            MessageBox.Show("Thank You");
+        }
+    }
+}
---
+++ EmailInterface/Student.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+
+namespace EmailInterface
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Mailid { get; set; }
+        public string Message { get; set; }
+
+
+        public void Mail(string mailad, string Message, string name, int rollno)
+        {
+
+            var fromAddress = new MailAddress("rahamath22104@gmail.com", "Rahamath");
+            var toAddress = new MailAddress(mailad, name);
+
+            const string fromPassword = "qzyk ddtt mpjz qefw";
+            const string subject = " Message";
+            string body = Message;
+
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                try
+                {
+                    System.Net.Mail.Attachment attachment;
+                    attachment = new System.Net.Mail.Attachment("D:\\Jaffersha\\" + rollno + ".txt");
+                    message.Attachments.Add(attachment);
+                }
+                catch (Exception ex) { }
+                smtp.Send(message);
+            }
+
+        }
+
+    }
+}
